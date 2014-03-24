24172841ba476588f2da28e37e090497531732cc
Hannah <hannah_wight@hotmail.com>
2014-03-24 18:22:08 +0000

    4 tests now working, can add link

--- server.rb
+++ server.rb
@@ -22,10 +22,12 @@ class BookmarkManager < Sinatra::Application
 	post '/links' do
 		url = params["url"]
 		title = params["title"]
+		
 		tags = params["tags"].split(" ").map do |tag|
-				Tag.first_or_create(:text => tag)
-			Link.create(:url => url, :title => title, :tags => tags)
-			redirect to('/')
+			Tag.first_or_create(:text => tag)
 		end
+
+		Link.create(:url => url, :title => title, :tags => tags)
+		redirect to('/')
 	end
 end
